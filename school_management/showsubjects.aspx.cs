46a5da189e3d6def3c31df062a2ff59fae791cc9
BhavyaArora <bhavya16arora@gmail.com>
2022-10-15 21:38:49 +0530

    Add project files.

---
+++ school_management/showsubjects.aspx.cs
@@ -0,0 +1,25 @@
+﻿using bal;
+using dataaccess;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace school_management
+{
+    public partial class showsubjects : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+          details_school b = new details_school();
+            List<subject> products = new List<subject>();
+            products = b.GetProducts1();
+
+            GridView1.DataSource = products;
+            GridView1.DataBind();
+
+        }
+    }
+}
